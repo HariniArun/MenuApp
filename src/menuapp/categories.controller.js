c49e956b9e014fb7fed97c34594044ac0616056b
Harini Arun Prasanna <arunprh@KAUST.EDU.SA>
2020-06-07 00:44:42 +0300

    new

---
+++ src/menuapp/categories.controller.js
@@ -0,0 +1,17 @@
+(function () {
+    'use strict';
+
+    angular.module('Data')
+    .controller('CategoriesController', CategoryController);
+
+
+    CategoryController.$inject = ['items'];
+    function CategoryController(items) {
+      var categories = this;
+      categories.items = items.data;
+      console.log(categories.items);
+
+
+    }
+
+    })();
